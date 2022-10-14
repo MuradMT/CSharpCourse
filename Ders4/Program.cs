192e2f2fad7c1f7f659d916f9c220c88b30a0292
Murad Mammadzade <mammadzade03@gmail.com>
2022-10-14 16:58:58 +0400

    Ders4

---
+++ Ders4/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ders4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //DRY
+            //Console.WriteLine(1);
+            //Console.WriteLine(2);
+            //Console.WriteLine(3);
+            //for(int i = 1; i < 4; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            //int a = ("Murad").Length;
+            //string some = "x";
+            //Console.WriteLine(some.Length);
+            //Console.WriteLine(a);
+            //for (int i = 1; i < 4; i--)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            //string x = "Murad";
+            //for (int i = 0; i <x.Length; i++)
+            //{
+            //    //Console.WriteLine(x[i]);
+            //    Console.Write($"{x[i]} ");
+            //}
+            //for+tab tab
+            //if+tab tab
+            //if (true)
+            //{
+
+            //}
+            //else if (true)
+            //{
+
+            //}
+            //i ayrica dovrlerde taninir ,eyni dovr icinde 2 defe olmur
+            //variable life cycle
+            //dovr deyiskenini iyle adlandir
+            //ic ice dovr
+            //ctrl k+d
+            //for (int i = 0; i <3; i++)
+            //{
+            //    for (int j= 0; j < 5; j++)
+            //    {
+            //        Console.WriteLine(j);
+            //    }
+            //}
+            //for (int i = 0; i < 5; i++)
+            //{
+
+            //}
+            //for (int i = 0; i <3; i++)
+            //{
+            //    for (int j = 0; j <4; j++)
+            //    {
+            //        Console.Write(j+" ");
+            //    }
+            //    Console.WriteLine();
+            //}
+            //for (int i = 0; i <5; i++)
+            //{
+            //    if (i == 3)
+            //    {
+            //        break;
+            //    }
+            //    Console.WriteLine(i);
+            //}
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    if (i == 3)
+            //    {
+            //        continue;
+            //    }
+            //    Console.WriteLine(i);
+            //}
+            //sonsuz dovr
+            //for (; ; )
+            //{
+            //    Console.WriteLine("k");
+            //}
+            //int i= 5;
+            //while (i < 10)
+            //{
+            //    Console.WriteLine(i);
+            //    i++;
+            //}
+            //int num=int.Parse(Console.ReadLine());
+            //while (num > 0)
+            //{
+            //    Console.WriteLine(num%10);
+            //    num/= 10;
+            //}
+            //while (true)
+            //{
+
+            //}
+            //string i = "5";
+            //int k = 0;
+            //do
+            //{
+            //    Console.WriteLine("Do you want continue?");
+            //    string x = Console.ReadLine();
+            //    if (x == "5")
+            //    {
+            //        k = int.Parse(x);
+
+            //    }
+            //} while (k > 0);
+
+            Console.ReadLine();
+        }
+    }
+}
