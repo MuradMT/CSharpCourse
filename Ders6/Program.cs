3b4401f8ed59f8fa67f9e2348372e319b9bc57e3
Murad Mammadzade <mammadzade03@gmail.com>
2022-10-21 23:27:37 +0400

    Ders6

---
+++ Ders6/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ders6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //var a=Console.Read();
+            //Console.WriteLine(a);
+            //Console.ReadLine();
+            //Console.ReadKey();
+            //Array bir data structuredir
+            //int[] numbers = new int[4];
+            //int[] numbers = new int[4]
+            //{
+            //    1,2,3,4
+            //};
+
+            //Console.WriteLine(numbers.Length);
+            //int n=int.Parse(Console.ReadLine());
+            //int[] num = new int[n];
+            //for (int i = 0; i < n; i++)
+            //{
+            //    num[i] = int.Parse(Console.ReadLine());
+            //}
+            //int[] numbers = { 1, 2, 3, 4 };
+            //Console.WriteLine(numbers[0]);
+            //Console.WriteLine(numbers[1]);
+            //Console.WriteLine(numbers[2]);
+            //Console.WriteLine(numbers[3]);
+            //for (int i = 0; i < n; i++)
+            //{
+            //    Console.WriteLine(num[i]);
+            //}
+            //string a = "Murad";
+            // new string();
+            //int c = 5;int b = 5;
+            //if (b==c)
+            //{
+            //    Console.WriteLine(true);
+            //}
+            //int[] num = { 3, 1, 6, 2, 8, 0 };
+            //int[] num1 = num;
+            //if (num == num1)
+            //{
+            //    Console.WriteLine(true);
+            //}
+            //Array.Sort(num);
+            //Console.WriteLine(num[0]);
+            //for (int i = num.Length-1; i >=0; i--)
+            //{
+            //    Console.WriteLine(num[i]);
+            //}
+            //value and reference types
+            //stack ve heap
+            //swap alghoritm 
+            //int helper = num[0];
+            //num[0] = num[1];
+            //num[1] = helper;
+            //Console.WriteLine(num[0]);
+            //Console.WriteLine(num[1]);
+            //one dimensional array
+            //int[] num = { 3, 1, 6, 2, 8, 0 };
+            //Array.Sort(num);
+            //Console.WriteLine(num[num.Length - 1]);
+            //Console.WriteLine(num.Max());
+            //Console.WriteLine(num.Min());
+            //Console.WriteLine(num.Sum());
+            //int count = 0;
+            //for (int i = 0; i < num.Length; i++)
+            //{
+            //    count += num[i];
+            //}
+            //Console.WriteLine(count);
+            //int[] arr = new int[4] { 1, 2, 3, 4 };
+            //int[] arr2 = new int[10];
+            
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+                
+            //    arr2[i] = arr[i];
+            //}
+            //for (int i = arr.Length; i < arr2.Length; i++)
+            //{
+            //    arr2[i]=int.Parse(Console.ReadLine());
+            //}
+            //Console.WriteLine("Cavab");
+            //for (int i = 0; i <arr2.Length; i++)
+            //{
+            //    Console.WriteLine(arr2[i]);
+            //}
+            Console.ReadLine();
+        }
+    }
+}
