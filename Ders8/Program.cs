ff2005be46d2f3eb6ab68fc60d7f6b0e6a6f1260
Murad Mammadzade <mammadzade03@gmail.com>
2022-11-01 14:09:47 +0400

    Ders8

---
+++ Ders8/Program.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ders8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] arr = { 1, 2, 3, 7, 2 };
+            //Array.Sort(arr);    
+            //int rec = arr[0];
+            //for (int i = 1; i < arr.Length; i++)
+            //{
+            //    if (arr[i] == rec)
+            //    {
+            //        arr[i] = -1;
+            //    }
+            //    else
+            //    {
+            //        rec = arr[i];
+            //    }
+            //}
+            //int count = 0;
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    if (arr[i] != -1)
+            //    {
+            //        count++;    
+            //    }
+            //}
+            //Console.WriteLine(count);
+            //var res=Console.ReadLine().Trim().Split();
+            //Console.WriteLine(res[res.Length-1].Length);
+            //int target = 5;
+            //int[] arr = new int[] { 1, 2, 3, 4, 5, 0 };
+            //int count = 0;
+            //for (int i = 0; i < arr.Length-1; i++)
+            //{
+            //    for (int j = i+1; j < arr.Length; j++)
+            //    {
+            //        if (arr[i] + arr[j]==target)
+            //            count++;
+            //    }
+            //}
+            //Console.WriteLine(count);
+            //int[] arr = new int[] { 11, 23, 31, 43 };
+            //bool a = false;
+            //for (int i = 0; i <arr.Length; i++)
+
+            //{
+            //    if (arr[i].ToString().Contains(i.ToString()))
+            //        a = true;
+
+            //}
+            //Console.WriteLine(a==true?"Ehtiva edir":"Ehtiva etmir");
+            //string ad = "Murad";
+            //var sira = ad.ToCharArray();
+            //Array.Sort(sira);
+            //Console.WriteLine(new string(sira));
+            //****************************************
+            //string some = "salamlar";
+            //int max = 0;
+            //    for (int i = 0; i < some.Length; i++)
+            //{
+            //    max++;
+            //}
+            //Console.WriteLine(max);
+            //string some = "ezize";
+            //bool deyer = true;
+            //for (int i = 0; i < some.Length/2; i++)
+            //{
+            //    if (some[i] != some[some.Length - 1 - i])
+            //    {
+            //        deyer = false;
+            //    }
+            //}
+            //Console.WriteLine(deyer);
+
+            //string z = "ezize";
+            //string ters = string.Empty;
+            //for (int i = z.Length-1; i >=0; i++)
+            //{
+            //    ters += z[i];
+            //}
+            //Console.WriteLine(z==ters?true:false);
+            #region Kenan
+
+            //string name =Console.ReadLine();
+            //var sep=Console.ReadLine();
+            //for(int i=0; i<name.Length; i++)
+            //{
+            //    if (i==name.Length-1)
+            //    {
+            //        Console.WriteLine(name[i]);
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine(name[i]+sep);
+            //    }
+            //}
+            #endregion
+            #region Cebrayil
+            //string a = Console.ReadLine();
+            //for (int i = 0; i < a.Length-1; i++)
+            //{
+            //    if (i == 0)
+            //    {
+            //        Console.Write(a[i]);
+            //    }
+            //    else
+            //    {
+            //        Console.Write(a[i].ToString().Insert(0, "!"));
+            //    }
+
+
+            //}
+            #endregion
+            #region Cavad
+            //string some=Console.ReadLine();
+            //var arr=some.ToCharArray();
+            //object sep = Console.ReadLine();
+            //for (int i = 0; i < arr.Length-1; i++)
+            //{
+            //    Console.WriteLine(arr[i]+sep.ToString());
+            //}
+            //Console.WriteLine(arr[arr.Length-1]);
+            #endregion
+            //String replace et
+            //string x = "Ananas";
+            //string y = "Salam";
+            //string result = string.Empty;
+            //for (int i = 0; i < x.Length; i++)
+            //{
+            //    if (x[i]=='A' || x[i] == 'a')
+            //    {
+            //        result += y;
+            //    }
+            //    else
+            //    {
+            //        result += x[i];
+            //    }
+            //}
+            //Console.WriteLine(result);
+            //Console.ReadLine();
+
+
+
+
+        }
+    }
+}
