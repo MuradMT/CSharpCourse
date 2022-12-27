62269c6760615491c9d662034641b1e6058388dc
Murad Mammadzade <mammadzade03@gmail.com>
2022-12-27 10:48:20 +0400

    Ders18

---
+++ Ders18/MyList.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ders18
+{
+    public class MyList<T>
+    {
+
+        //Generics-deyisenin tipini runtimede mueyyen edir
+        //mylist int,string,double
+        //code refactoring dogru olmayan-duzeltdik
+        T[] arr;
+        int Length { get; set; } = 0;
+        public MyList()
+        {
+            arr = new T[0];
+        }
+        public void Add(T data)
+        {
+            Length++;
+            T[] narr=new T[arr.Length];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                narr[i] = arr[i];
+            }
+            //0 1 2 - 3- 0 1 2 3
+            narr[Length-1] = data;
+            arr = narr;
+            //stack heap
+            //arr -  {1 2 3}-0x12   -orphan object Garbage Collector
+            //narr - {1,2,3,4}-0x13
+        }
+        public int Count 
+        {   
+            get { return Length; }
+        }
+        public void Remove(T data)
+        {
+            if (arr.Contains(data))
+            {
+                Length--;
+                T[] nnarr = new T[Length];
+                int count = 0;
+                int temp = 0;
+                for (int i = 0; i < arr.Length; i += 1)
+                {
+                    if (arr[i].Equals(data) && count==0)
+                    {
+                        count += 1;
+                    }
+                    else
+                    {
+                        nnarr[temp] = arr[i];
+                        temp++;
+                    }
+                }
+                arr = nnarr;
+            }
+        }
+        //equals ==
+        public T[] Data
+        {
+            get
+            {
+                return arr;
+            }
+        }
+        public bool Contains(T data)
+        {
+            return arr.Contains(data);
+        }
+        //indexer
+        public  T this[int index]//Mylist m=new Mylist
+            //m[2]
+        {
+            get
+            {
+                return arr[index];
+            }
+        }
+    }
+}
---
+++ Ders18/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+namespace Ders18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //List<int> list = new List<int>();
+            //list.Add(1);
+            //list.Add(2);
+            //list.Remove(1);
+            //Console.WriteLine(list.Count);
+            //Console.WriteLine(list[0]);
+            //foreach (var item in list)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            //---
+            //Add
+            //0-1 
+            MyList<int> myList = new MyList<int>();
+            myList.Add(1);
+            myList.Add(2);
+            myList.Add(3);
+            myList.Add(2);
+            myList.Remove(2);
+            WriteLine(myList.Count);
+            foreach (var item in myList.Data)
+            {
+                Console.WriteLine(item);
+            }
+
+            WriteLine(myList.Contains(5));
+
+            WriteLine(myList[1]);
+
+            MyList<string> list = new MyList<string>();
+            list.Add("kenan");
+            list.Add("Murad");
+            foreach (var item in list.Data)
+            {
+                Console.WriteLine(item);
+            }
+            ReadLine();
+        }
+    }
+}
